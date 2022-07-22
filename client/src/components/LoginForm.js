a0784e87f04b8c5f90058d26d8008c602618f831
sdmnny <sdmnny@gmail.com>
2022-07-22 17:54:21 -0400

    debugged final bits of code

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 // add useMutation
 import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
